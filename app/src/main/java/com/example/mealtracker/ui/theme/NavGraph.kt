8bf421d1c1ba11005ec4dfe5a6b4a4f7c8169c91
tristan <perry17522@gapps.uwcsea.edu.sg>
2023-10-31 11:10:07 +0800

    init#3

--- app/src/main/java/com/example/mealtracker/ui/theme/NavGraph.kt
+++ app/src/main/java/com/example/mealtracker/ui/theme/NavGraph.kt
@@ -6,12 +6,21 @@ import androidx.navigation.NavHostController
 
 annotation class composable
 
+sealed class Screen(val route: String){
+    object HomePage : Screen("Home_Page")
+    object HomePage2 : Screen("Home_Page#2")
+    object ProgressPage : Screen ("Progress_Page")
+    object ProgressPage2 : Screen ("Progress_Page#2")
+    object ArchivePage : Screen ("Archive_Page")
+
+}
+
 @composable
 fun NavGraph(navController: NavHostController){
     NavHost(
         navController = navController,
-        startDestination = screen.HomePage.route
+        startDestination = Screen.HomePage.route
     ){
-        composable(route = screen.)
+        composable(route = Screen.)
     }
 }
