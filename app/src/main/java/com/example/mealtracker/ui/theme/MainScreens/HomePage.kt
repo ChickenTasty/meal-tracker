4e96d64c500d954511865e7ed5197b1a3c22d421
tristan <perry17522@gapps.uwcsea.edu.sg>
2023-11-01 21:11:34 +0800

    init#8

--- app/src/main/java/com/example/mealtracker/ui/theme/MainScreens/HomePage.kt
+++ app/src/main/java/com/example/mealtracker/ui/theme/MainScreens/HomePage.kt
@@ -6,9 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
@@ -20,7 +17,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.ui.graphics.Color
@@ -28,10 +24,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import android.app.AlertDialog
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.platform.LocalContext
 import com.example.mealtracker.ui.theme.functions.screen
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.background
+import androidx.compose.material3.*
+import androidx.compose.foundation.layout.weight
+
+
 
 
 @Composable
@@ -80,7 +81,9 @@ Surface(
                     )
                 }
                 Button(
-                    onClick = {/*Navigate to ArchivePage*/  },
+                    onClick = {/*Navigate to ArchivePage*/
+                        navController.navigate(route = screen.ArchivePage.route)
+                              },
                     modifier = Modifier.padding(1.dp)
                 ) {
                     Text(
@@ -135,20 +138,18 @@ Surface(
                 .padding(8.dp)
         )
 
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(640.dp)
-                .background(Color.DarkGray, shape = RoundedCornerShape(9.dp))
-                .align(Alignment.BottomCenter)
+        LazyColumn(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ){
-            Text(
-                text = "Meals eaten today",
-                fontWeight = FontWeight.Bold,
-                fontSize = 26.sp,
-                color = Color.White
-            )
+            items(3){index ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .background(Color.LightGray),
+                    contentAlignment = Alignment.Center
+                )
+            }
         }
         openAddMeal()
     }
